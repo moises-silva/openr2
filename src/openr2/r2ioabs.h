b4b2f41e274e10c3ff954bd808a3a65d4aca1166
Moises Silva <moises.silva@gmail.com>
2009-01-13 02:55:58 +0000

    updated trunk library version and some misc changes to the build and r2ioabs.h
    git-svn-id: svn://libopenr2.org/openr2/trunk@141 c87312dc-f89b-4d16-81b4-35530099f826

--- src/openr2/r2ioabs.h
+++ src/openr2/r2ioabs.h
@@ -32,6 +32,14 @@ extern "C" {
 
 #include "r2exports.h"
 
+#define OR2_IO_READ  (1 << 0)
+#define OR2_IO_WRITE (1 << 1)
+#define OR2_IO_EVENT (1 << 2)
+
+typedef enum {
+	OR2_IO_EVENT_CAS_CHANGE,
+	OR2_IO_EVENT_ALARM
+} openr2_io_event_t;
 /* --custom-io (disables verification of presence of either zaptel, dahdi or openzap) 
  * and therefore an I/O interface definition should be provided at runtime
  * otherwise r2 channels cannot be created
@@ -44,6 +52,8 @@ int openr2_io_flush_write_buffers(openr2_chan_t *r2chan);
 int openr2_io_write(openr2_chan_t *r2chan, const void *buf, int size);
 int openr2_io_read(openr2_chan_t *r2chan, const void *buf, int size);
 int openr2_io_setup(openr2_chan_t *r2chan);
+int openr2_io_wait(openr2_chan_t *r2chan, int flags);
+openr2_io_event_t openr2_io_get_next_event(openr2_chan_t *r2chan);
 
 #if defined(__cplusplus)
 } /* endif extern "C" */
