6ea67808099da95dd63d3e8d5ed5a802076515cb
Moises Silva <moises.silva@gmail.com>
2009-02-03 18:15:04 +0000

    fixed small bug introduced when updating openr2 for ANATEL compliance
    git-svn-id: svn://libopenr2.org/openr2/branches/release-1@160 c87312dc-f89b-4d16-81b4-35530099f826

--- src/r2context.c
+++ src/r2context.c
@@ -648,6 +648,7 @@ int openr2_context_configure_from_advanced_file(openr2_context_t *r2context, con
 		}
 
 		/* Group A tones */
+		LOADTONE(mf_ga_tones.request_next_ani_digit)
 		LOADTONE(mf_ga_tones.request_next_dnis_digit)
 		LOADTONE(mf_ga_tones.request_dnis_minus_1)
 		LOADTONE(mf_ga_tones.request_dnis_minus_2)
