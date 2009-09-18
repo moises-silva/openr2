15ffc89c226cba4f621a06649aeb2b764bce106d
Moises Silva <moises.silva@gmail.com>
2009-09-18 04:00:13 +0000

    add copyright headers to new files
    git-svn-id: svn://libopenr2.org/openr2/branches/release-1-dtmf@247 c87312dc-f89b-4d16-81b4-35530099f826

--- misc/dtmf_detect.c
+++ misc/dtmf_detect.c
@@ -1,4 +1,27 @@
+/*
+ * OpenR2 
+ * MFC/R2 call setup library
+ *
+ * Moises Silva <moy@sangoma.com>
+ * Copyright (C) 2009 Moises Silva
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 /* compile with gcc dtmf_detect.c -o dtmf_detect.exe -lspandsp */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
