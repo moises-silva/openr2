ae8a6b0056a522537fbc0ec46f3bcc2b766ac4b0
Moises Silva <moises.silva@gmail.com>
2009-11-09 01:02:23 +0000

    updated asteriskr2 script
    git-svn-id: svn://libopenr2.org/openr2/trunk@272 c87312dc-f89b-4d16-81b4-35530099f826

--- debug/asterisk/astdumpr2.sh
+++ debug/asterisk/astdumpr2.sh
@@ -20,24 +20,36 @@ echo "== OpenR2 debugging information for Asterisk ==" > $1
 echo "OpenR2 reported version: " >> $1
 r2test -v >> $1
 
+echo -e "Asterisk version: " >> $1
+
+asterisk -rx 'core show version' >> $1
+
 echo -e "\nmfcr2 show version: " >> $1
 
 asterisk -rx 'mfcr2 show version' >> $1
 
-echo -e "\nmfcr2 show channels:\n" >> $1
-
-asterisk -rx 'mfcr2 show channels' > $1.channels.txt
-
-cat $1.channels.txt >> $1
-
-echo -e "\n$dahdi channels status:\n" >> $1
-
-for chan in `tail --lines=+2 $1.channels.txt | awk '{print $1}'`
+for t in 1 2
 do
-	asterisk -rx "dahdi show channel $chan" >> $1
-done
+	echo "" >> $1
+	echo "== Data Collection $t ==" >> $1
+	echo "" >> $1
 
-rm $1.channels.txt
+	echo -e "\nmfcr2 show channels:\n" >> $1
+	
+	asterisk -rx 'mfcr2 show channels' > $1.channels.txt
+	
+	cat $1.channels.txt >> $1
+	
+	echo -e "\n$dahdi channels status:\n" >> $1
+	
+	for chan in `tail --lines=+2 $1.channels.txt | awk '{print $1}'`
+	do
+		asterisk -rx "dahdi show channel $chan" >> $1
+	done
+	
+	rm $1.channels.txt
+	sleep 2s
+done
 
 set +x
 
